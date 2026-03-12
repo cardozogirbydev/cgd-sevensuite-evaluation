4c60aa885dd96142d13b6274bdfc728f93a714ab
cardozogirbydev <200120836+cardozogirbydev@users.noreply.github.com>
2026-03-12 02:32:20 -0400

    feat: update database schema and seed data for users and civil status

--- sql/01_SchemaCreation.sql
+++ sql/01_SchemaCreation.sql
@@ -6,7 +6,7 @@ USE [SevenSuiteDB];
 
 CREATE TABLE [ESTADO_CIVIL]
 (
-  [id_estado]   INT PRIMARY KEY IDENTITY(1, 1),
+  [id_estado_civil]   INT PRIMARY KEY IDENTITY(1, 1),
   [descripcion] VARCHAR(50) NOT NULL
 );
 
@@ -17,5 +17,75 @@ CREATE TABLE [SEVECLIE]
   [nombre]          VARCHAR(100) NOT NULL,
   [genero]          CHAR(1) CONSTRAINT [CHK_genero] CHECK([genero] IN('M', 'F')),
   [fecha_nac]       DATE NOT NULL,
-  [id_estado_civil] INT FOREIGN KEY REFERENCES [ESTADO_CIVIL]([id_estado])
+  [id_estado_civil] INT FOREIGN KEY REFERENCES [ESTADO_CIVIL]([id_estado_civil])
 );
+
+CREATE TABLE [USUARIO]
+(
+  [id_usuario] INT PRIMARY KEY IDENTITY(1, 1),
+  [username]   VARCHAR(50) NOT NULL UNIQUE,
+  [password]   VARCHAR(255) NOT NULL
+);
+
+CREATE PROCEDURE [usp_ClientsGet]
+  @Filtro VARCHAR(100) = NULL
+AS
+BEGIN
+  SELECT
+    CL.[id_clie],
+    CL.[cedula],
+    CL.[nombre],
+    CL.[genero],
+    CL.[fecha_nac],
+    EC.[id_estado_civil],
+    EC.[descripcion] AS [desc_estado_civil]
+  FROM [dbo].[SEVECLIE] CL
+    INNER JOIN [dbo].[ESTADO_CIVIL] EC ON CL.[id_estado_civil] = EC.[id_estado_civil]
+  WHERE (@Filtro IS NULL OR CL.[nombre] LIKE '%' + @Filtro + '%' OR CL.[cedula] LIKE '%' + @Filtro + '%')
+  ORDER BY CL.[nombre] ASC
+END;
+
+CREATE PROCEDURE [usp_ClientUpsert]
+  @id_clie          INT,
+  @cedula           VARCHAR(20),
+  @nombre           VARCHAR(100),
+  @genero           CHAR(1),
+  @fecha_nac        DATE,
+  @id_estado_civil  INT
+AS
+BEGIN
+  MERGE INTO [dbo].[SEVECLIE] AS [Target]
+  USING(SELECT @id_clie AS [id_clie]) AS [Source]
+  ON [Target].[id_clie] = [Source].[id_clie] AND [Source].[id_clie] > 0
+  WHEN MATCHED THEN
+    UPDATE SET
+      [cedula] = @cedula,
+      [nombre] = @nombre,
+      [genero] = @genero,
+      [fecha_nac] = @fecha_nac,
+      [id_estado_civil] = @id_estado_civil
+  WHEN NOT MATCHED THEN
+    INSERT
+    (
+      [cedula],
+      [nombre],
+      [genero],
+      [fecha_nac],
+      [id_estado_civil]
+    )
+    VALUES(
+      @cedula,
+      @nombre,
+      @genero,
+      @fecha_nac,
+      @id_estado_civil
+    );
+END;
+
+CREATE PROCEDURE [usp_ClientDelete]
+  @id_clie INT
+AS
+BEGIN
+  DELETE FROM [dbo].[SEVECLIE]
+  WHERE [id_clie] = @id_clie;
+END;
--- sql/02_SeedData.sql
+++ sql/02_SeedData.sql
@@ -1,31 +1,39 @@
 USE [SevenSuiteDB];
 
+-- ESTADO_CIVIL
 DECLARE 
-	@desc_soltero VARCHAR(50) = 'soltero(a)',
-	@desc_casado VARCHAR(50) = 'casado(a)',
-	@desc_divorciado VARCHAR(50) = 'divorciado(a)',
-	@desc_viudo VARCHAR(50) = 'viudo(a)';
+  @desc_soltero VARCHAR(50) = 'soltero(a)',
+  @desc_casado VARCHAR(50) = 'casado(a)',
+  @desc_divorciado VARCHAR(50) = 'divorciado(a)',
+  @desc_viudo VARCHAR(50) = 'viudo(a)';
 
 IF NOT EXISTS(SELECT 1 FROM [dbo].[ESTADO_CIVIL] WHERE [descripcion] = @desc_soltero)
 BEGIN
-	INSERT INTO [dbo].[ESTADO_CIVIL]([descripcion])
-	VALUES(@desc_soltero);
+  INSERT INTO [dbo].[ESTADO_CIVIL]([descripcion])
+  VALUES(@desc_soltero);
 END;
 
 IF NOT EXISTS(SELECT 1 FROM [dbo].[ESTADO_CIVIL] WHERE [descripcion] = @desc_casado)
 BEGIN
-	INSERT INTO [dbo].[ESTADO_CIVIL]([descripcion])
-	VALUES(@desc_casado);
+  INSERT INTO [dbo].[ESTADO_CIVIL]([descripcion])
+  VALUES(@desc_casado);
 END;
 
 IF NOT EXISTS(SELECT 1 FROM [dbo].[ESTADO_CIVIL] WHERE [descripcion] = @desc_divorciado)
 BEGIN
-	INSERT INTO [dbo].[ESTADO_CIVIL]([descripcion])
-	VALUES(@desc_divorciado);
+  INSERT INTO [dbo].[ESTADO_CIVIL]([descripcion])
+  VALUES(@desc_divorciado);
 END;
 
 IF NOT EXISTS(SELECT 1 FROM [dbo].[ESTADO_CIVIL] WHERE [descripcion] = @desc_viudo)
 BEGIN
-	INSERT INTO [dbo].[ESTADO_CIVIL]([descripcion])
-	VALUES(@desc_viudo);
+  INSERT INTO [dbo].[ESTADO_CIVIL]([descripcion])
+  VALUES(@desc_viudo);
+END;
+
+-- USUARIOS
+IF NOT EXISTS(SELECT 1 FROM [dbo].[USUARIO] WHERE [username] = 'admin')
+BEGIN
+  INSERT INTO [dbo].[USUARIO]([username], [password])
+  VALUES('admin', CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', 'admin123'), 2));
 END;
